d9eb151bfa4c3f1b680ece1015ec6db07790d367
huyphamsofteng <do.giahuy.pham@gmail.com>
2024-03-23 11:38:32 -0400

    Add GUI for index and sql configure

--- app.py
+++ app.py
@@ -5,28 +5,29 @@ from flask import abort, redirect, url_for
 from helper import *
 
 app = Flask(__name__)
-stocks = [
-    {
-    "symbol": "VOO",
-    "shares": "10",
-    "book_value": "book_value",
-    "price": "456"
-},
-{
-    "symbol": "APPL",
-    "shares": "5",
-    "book_value": "book_value",
-    "price": "478"
-}
-]
+stocks = []
 stock = {
     "symbol": "symbol",
     "shares": "shares",
     "book_value": "book_value",
-    "price": "price"
+    "market_price": "market_price"
 }
 @app.route("/", methods=['POST', 'GET'])
 def index():
+    stocks = [
+        {
+        "symbol": "VOO",
+        "shares": "10",
+        "book_value": "350",
+        "market_price": "456"
+        },
+        {
+        "symbol": "APPL",
+        "shares": "5",
+        "book_value": "375",
+        "market_price": "478"
+        }
+    ]
     return render_template('index.html', stocks = stocks),200
 
 @app.route("/buy", methods=['POST', 'GET'])
--- helper.py
+++ helper.py
@@ -1,5 +1,18 @@
 import yfinance as yf
-import requests 
+import requests
+
+import mysql.connector
+
+def create_db():
+    cnx = mysql.connector.connect(user='dpham', password='0918', host='127.0.0.1', database='stockbase')
+    cursor = cnx.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS stockbase")
+    cursor.execute("CREATE TABLE IF NOT EXISTS stock (id INT(100) auto_increment, symbol CHAR(4), book_value FLOAT(7), market_value FLOAT(7), shares INT(100), user_id INT(25), PRIMARY KEY (id), FOREIGN KEY (user_id) REFERENCES user(user_id))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS user (user_id INT(100) auto_increment,user_name CHAR(10),user_password CHAR(10),total_value FLOAT(7),PRIMARY KEY (user_id))")
+    print(cnx)
+    cnx.commit()
+    cnx.close()
+    
 def check(symbol):
     try:
         response = requests.get(f'https://query2.finance.yahoo.com/v10/finance/quoteSummary/AAAA?modules=financialData%2CquoteType%2CdefaultKeyStatistics%2CassetProfile%2CsummaryDetail&corsDomain=finance.yahoo.com&formatted=false&symbol={symbol}&crumb=P8H2Ln3T.n2')
